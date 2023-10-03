e5974fb4e67866d9f92dee6efcab69cdf4f5f2ce
Martin Uzunuv <uzunovm0@gmail.com>
2023-10-03 12:12:41 +0300

    init

--- src/Az.java
+++ src/Az.java
@@ -4,25 +4,64 @@ import java.util.Timer;
 import java.util.TimerTask;
 
 public class Az extends JPanel {
-    //Here you declare variables that you want to use in the repeating function
-    private double b = 1;
 
-    private double c = 1;
+    int windowWidth = 600;
+    int windowHeight = 600;
+    int periodMS = 10;
+
+    //Here you declare variables that you want to use in the repeating function
+
+    //-------------------------------------------------------------------------------->
+    //📃 DECLARE VARIABLES HERE------------------------------------------------------->
+
+    double x = 10;
+    double y = 200;
+    double xSpeed = 1.5;
+    double ySpeed = 1.5;
+    Color color = new Color(100,20,150,200);
+
+    //END DECLARE VARIABLES HERE------------------------------------------------------>
+    //-------------------------------------------------------------------------------->
 
     public Az() {
         // Set preferred size for the custom component
-        setPreferredSize(new Dimension(600, 600));
+        setPreferredSize(new Dimension(windowWidth, windowHeight));
 
         Timer timer = new Timer();
         timer.scheduleAtFixedRate(new TimerTask() {
             @Override
             public void run() {
                 //This function will be executed periodically
-                b += 0.0005;
-                c -= 0.0005;
+
+                //----------------------------------------------------------------------------->
+                //🔁 REPEATING FUNCTION-------------------------------------------------------->
+
+                if(x < 0){
+                    xSpeed = 1.5;
+                    color = Color.BLUE;
+                }
+                if(y < 0){
+                    ySpeed = 1.5;
+                    color = Color.RED;
+                }
+                if(x+50 > 600){
+                    xSpeed = -1.5;
+                    color = Color.GREEN;
+                }
+                if(y+50 > 600){
+                    ySpeed = -1.5;
+                    color = Color.BLACK;
+                }
+
+                x+=xSpeed;
+                y+=ySpeed;
+
+                //END REPEATING FUNCTION------------------------------------------------------->
+                //----------------------------------------------------------------------------->
+
                 repaint();
             }
-        }, 0, 10); //1000 milliseconds = 1 second
+        }, 0, periodMS); //1000 milliseconds = 1 second
     }
 
     @Override
@@ -31,72 +70,17 @@ public class Az extends JPanel {
 
         //Here you draw and place your main logic
 
-//        for(double i = 0; i < Math.PI*4; i+= Math.PI/2) {
-//            g.setColor(Color.RED);
-//            for (double a = 0; a < Math.PI * 4; a += Math.PI / 20) {
-//                double doubleX = 300 + Math.cos(a + b + i) * a * 25;
-//                int x = (int) doubleX;
-//                double doubleY = 300 + Math.sin(a + b + i) * a * 25;
-//                int y = (int) doubleY;
-//                g.fillRect(x, y, 2, 2);
-//                g.setColor(Color.BLACK);
-//                for (double a2 = 0; a2 < Math.PI * 2; a2 += Math.PI / 40) {
-//                    double doubleX2 = doubleX + Math.cos(a2)* 10;
-//                    int x2 = (int) doubleX2;
-//                    double doubleY2 = doubleY + Math.sin(a2) * 10;
-//                    int y2 = (int) doubleY2;
-//                    g.fillRect(x2, y2, 1, 1);
-//
-//                }
-//            }
-//        }
+        //-------------------------------------------------------------------------------->
+        //🧠🎨 DRAW AND WRITE MAIN LOGIC HERE--------------------------------------------->
 
-        Color customColor = new Color(255, 0, 0, 40);
-        Color customColor2 = new Color(0, 0, 255, 40);
-        Color customColor3 = new Color(0, 255, 0, 40);
-        Color customColor4 = new Color(255, 255, 255, 40);
-        g.setColor(Color.BLACK);
-        g.fillRect(0, 0, 600, 600);
-        for(double a2 = 0; a2 < Math.PI*2; a2+=Math.PI*2/4) {
-            g.setColor(customColor);
-            for (double a = 0; a < Math.PI * 2; a += Math.PI / 100) {
-                double doubleX = 300 + Math.cos(a + Math.PI * 1.5 + a2) * 200;
-                int x = (int) doubleX;
-                double doubleY = 300 + Math.sin(a + Math.PI * 1.5+a2) * 200;
-                int y = (int) doubleY;
-                double doubleX2 = 300 + Math.cos(a * b + Math.PI * 1.5 + a2) * 200;
-                int x2 = (int) doubleX2;
-                double doubleY2 = 300 + Math.sin(a * b + Math.PI * 1.5 + a2) * 200;
-                int y2 = (int) doubleY2;
-                g.drawLine(x, y, x2, y2);
-            }
+        g.setColor(color);
+        int xToInt = (int) x;
+        int yToInt = (int) y;
+        g.fillRect(xToInt,yToInt,50,50);
 
-            g.setColor(customColor2);
-            for (double a = 0; a < Math.PI * 2; a += Math.PI / 100) {
-                double doubleX = 300 + Math.cos(a + Math.PI * 1.5 + a2) * 200;
-                int x = (int) doubleX;
-                double doubleY = 300 + Math.sin(a + Math.PI * 1.5 + a2) * 200;
-                int y = (int) doubleY;
-                double doubleX2 = 300 + Math.cos(a * c + Math.PI * 1.5 + a2) * 200;
-                int x2 = (int) doubleX2;
-                double doubleY2 = 300 + Math.sin(a * c + Math.PI * 1.5 + a2) * 200;
-                int y2 = (int) doubleY2;
-                g.drawLine(x, y, x2, y2);
-            }
+        //END DRAW AND WRITE MAIN LOGIC HERE---------------------------------------------->
+        //-------------------------------------------------------------------------------->
 
-//            g.setColor(customColor3);
-//            for (double a = 0; a < Math.PI * 2; a += Math.PI / 50) {
-//                double doubleX = 300 + Math.cos(a + Math.PI * 1.2 + a2) * 200;
-//                int x = (int) doubleX;
-//                double doubleY = 300 + Math.sin(a + Math.PI * 1.2 + a2) * 200;
-//                int y = (int) doubleY;
-//                double doubleX2 = 300 + Math.cos(a * c + Math.PI * 1.2 + a2) * 200;
-//                int x2 = (int) doubleX2;
-//                double doubleY2 = 300 + Math.sin(a * c + Math.PI * 1.2 + a2) * 200;
-//                int y2 = (int) doubleY2;
-//                g.drawLine(x, y, x2, y2);
-//            }
-        }
     }
 
     public static void main(String[] args) {
--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.awt.*;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
@@ -6,12 +8,77 @@ public class Main {
         // IntelliJ IDEA suggests fixing it.
         System.out.printf("Hello and welcome!");
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        //Examples
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+        //1---------->
+        //        for(double i = 0; i < Math.PI*4; i+= Math.PI/2) {
+//            g.setColor(Color.RED);
+//            for (double a = 0; a < Math.PI * 4; a += Math.PI / 20) {
+//                double doubleX = 300 + Math.cos(a + b + i) * a * 25;
+//                int x = (int) doubleX;
+//                double doubleY = 300 + Math.sin(a + b + i) * a * 25;
+//                int y = (int) doubleY;
+//                g.fillRect(x, y, 2, 2);
+//                g.setColor(Color.BLACK);
+//                for (double a2 = 0; a2 < Math.PI * 2; a2 += Math.PI / 40) {
+//                    double doubleX2 = doubleX + Math.cos(a2)* 10;
+//                    int x2 = (int) doubleX2;
+//                    double doubleY2 = doubleY + Math.sin(a2) * 10;
+//                    int y2 = (int) doubleY2;
+//                    g.fillRect(x2, y2, 1, 1);
+//
+//                }
+//            }
+//        }
+
+
+        //2------------>
+
+//        Color customColor = new Color(255, 0, 0, 40);
+//        Color customColor2 = new Color(0, 0, 255, 40);
+//        Color customColor3 = new Color(0, 255, 0, 40);
+//        Color customColor4 = new Color(255, 255, 255, 40);
+//        g.setColor(Color.BLACK);
+//        g.fillRect(0, 0, 600, 600);
+//        for(double a2 = 0; a2 < Math.PI*2; a2+=Math.PI*2/4) {
+//            g.setColor(customColor);
+//            for (double a = 0; a < Math.PI * 2; a += Math.PI / 100) {
+//                double doubleX = 300 + Math.cos(a + Math.PI * 1.5 + a2) * 200;
+//                int x = (int) doubleX;
+//                double doubleY = 300 + Math.sin(a + Math.PI * 1.5+a2) * 200;
+//                int y = (int) doubleY;
+//                double doubleX2 = 300 + Math.cos(a * b + Math.PI * 1.5 + a2) * 200;
+//                int x2 = (int) doubleX2;
+//                double doubleY2 = 300 + Math.sin(a * b + Math.PI * 1.5 + a2) * 200;
+//                int y2 = (int) doubleY2;
+//                g.drawLine(x, y, x2, y2);
+//            }
+//
+//            g.setColor(customColor2);
+//            for (double a = 0; a < Math.PI * 2; a += Math.PI / 100) {
+//                double doubleX = 300 + Math.cos(a + Math.PI * 1.5 + a2) * 200;
+//                int x = (int) doubleX;
+//                double doubleY = 300 + Math.sin(a + Math.PI * 1.5 + a2) * 200;
+//                int y = (int) doubleY;
+//                double doubleX2 = 300 + Math.cos(a * c + Math.PI * 1.5 + a2) * 200;
+//                int x2 = (int) doubleX2;
+//                double doubleY2 = 300 + Math.sin(a * c + Math.PI * 1.5 + a2) * 200;
+//                int y2 = (int) doubleY2;
+//                g.drawLine(x, y, x2, y2);
+//            }
+
+//            g.setColor(customColor3);
+//            for (double a = 0; a < Math.PI * 2; a += Math.PI / 50) {
+//                double doubleX = 300 + Math.cos(a + Math.PI * 1.2 + a2) * 200;
+//                int x = (int) doubleX;
+//                double doubleY = 300 + Math.sin(a + Math.PI * 1.2 + a2) * 200;
+//                int y = (int) doubleY;
+//                double doubleX2 = 300 + Math.cos(a * c + Math.PI * 1.2 + a2) * 200;
+//                int x2 = (int) doubleX2;
+//                double doubleY2 = 300 + Math.sin(a * c + Math.PI * 1.2 + a2) * 200;
+//                int y2 = (int) doubleY2;
+//                g.drawLine(x, y, x2, y2);
+//            }
         }
-    }
+
 }
